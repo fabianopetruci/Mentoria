d19cf3bdf2e04f79277f6f5f94dd059059abe301
fabianopetruci <fabianopetruci@gmail.com>
2026-03-04 15:17:48 -0300

    feat(recibos): add recibos module route and replace logout card on dashboard

--- js/core/router.js
+++ js/core/router.js
@@ -14,6 +14,7 @@ window.Router = {
     "pendencias",
     "contrato",
     "galeria",
+    "recibos",
   ],
 
   init() {
--- js/modules/home.js
+++ js/modules/home.js
@@ -20,7 +20,7 @@ window.Home = {
       { label: "CONTRATO", route: "contrato", img: "contract.png" },
       { label: "FLUXO DE CAIXA", route: "financeiro", img: "cash_flow.png" },
       { label: "MURAL DE FOTOS", route: "galeria", img: "gallery.png" },
-      { label: "SAIR DO SISTEMA", route: "logout", img: "logout.png" },
+      { label: "RECIBOS", route: "recibos", img: "receipt.png" },
     ];
 
     homeSection.innerHTML = `
